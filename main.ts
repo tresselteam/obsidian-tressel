8709f0f757d3b3e65b2de38c4ad6412a505402d0
aseem-thakar <41178612+aseem-thakar@users.noreply.github.com>
2021-11-21 15:22:17 +1100

    feat: sync tressel function

--- main.ts
+++ main.ts
@@ -1,17 +1,30 @@
-import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
-
-// Remember to rename these classes and interfaces!
+import axios from "axios";
+import {
+	App,
+	DataAdapter,
+	normalizePath,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import sanitize from "sanitize-filename";
 
 interface TresselPluginSettings {
 	tresselUserToken: string;
+	tweetsToIgnore: Array<string>;
+	threadsToIgnore: Array<string>;
 }
 
 const DEFAULT_SETTINGS: TresselPluginSettings = {
 	tresselUserToken: "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+	tweetsToIgnore: [],
+	threadsToIgnore: [],
 };
 
 export default class TresselPlugin extends Plugin {
 	settings: TresselPluginSettings;
+	fs: DataAdapter;
 
 	async onload() {
 		await this.loadSettings();
@@ -20,10 +33,102 @@ export default class TresselPlugin extends Plugin {
 		this.addRibbonIcon("sync", "Sync Tressel", (evt: MouseEvent) => {
 			// Called when the user clicks the button.
 			new Notice("Clicked Sync Tressel");
+			this.syncTressel().then(() => {
+				this.saveSettings();
+			});
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new TresselSettingTab(this.app, this));
+	}
+
+	async syncTressel() {
+		if (
+			this.settings.tresselUserToken !==
+			"XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+		) {
+			this.fs = this.app.vault.adapter;
+			// Get the user's tweets and threads by their token
+			const userData = await axios.get(
+				`https://us-central1-tressel-646e8.cloudfunctions.net/getUserDataByObsidianToken?token=${this.settings.tresselUserToken}`
+			);
+
+			// Create the Tressel folder if it doesn't already exist
+			const tresselFolderExists = await this.fs.exists(
+				normalizePath("/🗃️ Tressel")
+			);
+
+			if (!tresselFolderExists) {
+				await this.app.vault.createFolder("/🗃️ Tressel");
+			}
+
+			if (userData.data.tweets.length !== 0) {
+				for (let tweet of userData.data.tweets) {
+					// Check if tweets have been added in already
+					if (!this.settings.tweetsToIgnore.includes(tweet.id)) {
+						// Otherwise create new page for them in Tressel directory
+						let template = [
+							`# ${tweet.text.slice(0, 50)}\n`,
+							`## Metadata\n`,
+							`- Author: [${tweet.author.name}](${tweet.author.url})`,
+							`- Type: Thread`,
+							`- URL: ${tweet.url}\n`,
+							`## Thread\n`,
+							`${tweet.text}`,
+						].join("\n");
+
+						this.fs
+							.write(
+								`${normalizePath(
+									"/🗃️ Tressel/" +
+										sanitize(tweet.text.slice(0, 50)) +
+										".md"
+								)}`,
+								template
+							)
+							.then(async () => {
+								this.settings.tweetsToIgnore.push(tweet.id);
+							});
+					}
+				}
+			}
+
+			if (userData.data.threads.length !== 0) {
+				for (let thread of userData.data.threads) {
+					// Check if threads have been added in already
+					if (!this.settings.threadsToIgnore.includes(thread.id)) {
+						// Otherwise create new page for them in Tressel directory
+						let template = [
+							`# ${thread.fullThreadText[0].slice(0, 50)}\n`,
+							`## Metadata\n`,
+							`- Author: [${thread.author.name}](${thread.author.url})`,
+							`- Type: Thread`,
+							`- URL: ${thread.threadUrl}\n`,
+							`## Thread\n`,
+							`${thread.fullThreadText.join("\n\n")}`,
+						].join("\n");
+
+						this.fs
+							.write(
+								`${normalizePath(
+									"/🗃️ Tressel/" +
+										sanitize(
+											thread.fullThreadText[0].slice(
+												0,
+												50
+											)
+										) +
+										".md"
+								)}`,
+								template
+							)
+							.then(() => {
+								this.settings.threadsToIgnore.push(thread.id);
+							});
+					}
+				}
+			}
+		}
 	}
 
 	onunload() {}
